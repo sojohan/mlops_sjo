31a111435ab7802625acb526d6e23b3d0751e64b
sojohan <sojohan@users.noreply.github.com>
2020-09-09 08:06:21 +0200

    Update test.py

--- code/test/test.py
+++ code/test/test.py
@@ -6,7 +6,7 @@ from azureml.core import Webservice
 def main(service):
     # Creating input data
     print("Creating input data")
-    data = {"data": [[ 1,2,3,4 ], [ 10,9,8,7 ]]}
+    data = {"data": [[ 1,2,3,4 ], [ 10,9,8,8 ]]}
     input_data = json.dumps(data)
 
     # Calling webservice
